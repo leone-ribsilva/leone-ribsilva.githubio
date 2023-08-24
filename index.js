a63ecc6654e4bd914df4270352cfe64f48c86849
Leone Ribeiro Silva <91298561+leone-ribsilva@users.noreply.github.com>
2023-08-23 21:54:42 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const monthList = document.getElementById('month-list')
-    const addExpenseButton = document.getElementById('add-expense')
+    const addExpenseButton = document.querySelector('a[href="expenses.html"]')
 
-    const expenses = []
+    let expenses = []
 
     function calculateTotal(month) {
         return expenses.reduce((total, expense) => {
@@ -21,15 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
     function createMonthItem(month) {
         const total = calculateTotal(month)
         if (total > 0) {
-            const li = document.createElement('li')
-            li.textContent = month
-            monthList.appendChild(li)
+            const row = monthList.insertRow()
+            const monthCell = row.insertCell(0)
+            const totalCell = row.insertCell(1)
+            monthCell.textContent = month
+            totalCell.textContent = formatCurrency(total)
         }
     }
 
     monthList.addEventListener('click', function (event) {
-        if (event.target.tagName === 'LI') {
-            const selectedMonth = event.target.textContent
+        if (event.target.tagName === 'TD') {
+            const selectedMonth =
+                event.target.parentElement.firstElementChild.textContent
             showTotal(selectedMonth)
         }
     })
@@ -52,40 +55,6 @@ document.addEventListener('DOMContentLoaded', function () {
         'Novembro',
         'Dezembro'
     ]
-    months.forEach(createMonthItem)
-
-    // Função para atualizar a tabela de despesas por mês
-    function updateExpensesByMonthTable() {
-        const expensesByMonthTable = document
-            .getElementById('expenses-by-month')
-            .getElementsByTagName('tbody')[0]
-        expensesByMonthTable.innerHTML = ''
-
-        const expensesByMonth = calculateExpensesByMonth(expenses)
-
-        for (const [month, total] of Object.entries(expensesByMonth)) {
-            const row = expensesByMonthTable.insertRow()
-            const monthCell = row.insertCell(0)
-            const totalCell = row.insertCell(1)
-            monthCell.textContent = month
-            totalCell.textContent = formatCurrency(total)
-        }
-    }
-
-    // Função para calcular despesas por mês
-    function calculateExpensesByMonth(expenses) {
-        const expensesByMonth = {}
-
-        for (const expense of expenses) {
-            if (expensesByMonth[expense.month]) {
-                expensesByMonth[expense.month] += expense.value
-            } else {
-                expensesByMonth[expense.month] = expense.value
-            }
-        }
-
-        return expensesByMonth
-    }
 
     // Função para formatar valor como moeda
     function formatCurrency(value) {
@@ -95,6 +64,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }).format(value)
     }
 
-    // Chamar a função para atualizar a tabela inicialmente
-    updateExpensesByMonthTable()
+    // Restante do código para carregar despesas da planilha
+    const url =
+        'https://script.google.com/macros/s/AKfycbzmylTHNOaxhvf8GwzBDO6FIP6tfbNY-WwT-4jB9JJw4tJuqQbx3IMwU82SKRg9opkq/exec'
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                expenses = data.data.map(expense => ({
+                    ...expense,
+                    value: parseFloat(expense.value),
+                    month: expense.date.split('/')[1]
+                }))
+
+                months.forEach(createMonthItem)
+            } else {
+                console.error('Erro ao buscar despesas')
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })
 })
